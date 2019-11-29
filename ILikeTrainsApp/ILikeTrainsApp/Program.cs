858eae8e9bcae3fe5b1a5c85d7690c3923ed5850
Hangull <mz.piech@gmail.com>
2019-11-29 08:45:03 +0100

    Stuff

--- ILikeTrainsApp/ILikeTrainsApp/Program.cs
+++ ILikeTrainsApp/ILikeTrainsApp/Program.cs
@@ -6,7 +6,30 @@ namespace ILikeTrainsApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("I Like Trains!");
+
+            int aNumber = 0;
+            int numberOfLikes = 0;
+            string readALine;
+
+            do
+            {
+                Console.WriteLine("Press 1 to add like to train!");
+                Console.WriteLine("Press 3 to Exit!");
+                readALine = Console.ReadLine();
+
+                if(readALine=="1")
+                {
+                    numberOfLikes++;
+                }
+                if(readALine=="3")
+                {
+                    break;
+                }
+            }
+            while (aNumber == 5);
+
+
         }
     }
 }
